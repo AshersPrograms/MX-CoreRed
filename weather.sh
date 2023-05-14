06123a66e4e4230cfc3a614adfc08af5f56bbfe8
Asher's Programs <117654867+AshersPrograms@users.noreply.github.com>
2023-05-14 02:50:45 -0700

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -3,19 +3,31 @@
 #
 #USAGE: weather.sh <locationcode>
 #USAGE: weather.sh 	#the program will look up the weather for you
+#USAGE: replace with your zip code line YOURZIPCODE 
 #
-#(c) Michael Seiler 2007
+#examples
+# weather.sh
+# weather.sh 85076
+# change YOURZIPCODE to your zip code and run the program again
+# weather.sh
+# Author: (c) Michael Seiler 2007
 # Additional Author: Asher Simcha ashersprograms@gmail.com
+# Date: 05-12-2023
+# Last_Modified: 05-14-2023
 METRIC=0 #Should be 0 or 1; 0 for F, 1 for C
-# Get public IP address
-if [ -z $1 ]; then
-	public_ip=$(curl -s https://api.ipify.org)
-	# Use IP address to get geolocation information
-	response=$(curl -s "https://ipapi.co/$public_ip/json/")
-	# Extract zip code from the response
-	CITY=$(echo "$response" | jq -r '.postal')
+YOURZIPCODE=null
+if [ "$YOURZIPCODE" != "null" ]; then
+  CITY=$YOURZIPCODE
 else
-	CITY=$1
+	if [ -z $1 ]; then
+		# Get public IP address
+		public_ip=$(curl -s https://api.ipify.org)
+		# Use IP address to get geolocation information
+		response=$(curl -s "https://ipapi.co/$public_ip/json/")
+		# Extract zip code from the response
+		CITY=$(echo "$response" | jq -r '.postal')
+	else
+		CITY=$1
+	fi
 fi
 curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$CITY | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
-echo
